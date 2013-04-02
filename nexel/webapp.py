92d2994eb4bd796e1aedb4e53006d85c1a6efacc
Uli Felzmann <ulrich.felzmann@gmail.com>
2013-04-02 16:22:11 +1100

    added backwards compabilitly for tornado logger

--- nexel/webapp.py
+++ nexel/webapp.py
@@ -1,15 +1,19 @@
-import logging
-from nexel.config.settings import Settings
-from nexel.handlers import dispatcher
-import tornado.options
-import tornado.web
-
 DEBUG = True
 
 
 def install():
+    import logging
+    from nexel.config.settings import Settings
+    from nexel.handlers import dispatcher
+    from tornado import version_info
+    if version_info[0] >= 3:
+        import tornado.log
+        tornado.log.enable_pretty_logging()
+    else:
+        import tornado.options
+        tornado.options.enable_pretty_logging()
+    import tornado.web
     web_app = tornado.web.Application(dispatcher, debug=DEBUG)
     web_app.listen(Settings()['server_port'])
-    tornado.options.enable_pretty_logging()
     logging.getLogger().setLevel(logging.INFO)
     return web_app
