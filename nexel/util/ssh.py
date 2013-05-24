58c41f7122ab4f9b31c5ffbc7081872734c1fcc1
portrain <port.rain@gmail.com>
2013-05-24 11:35:17 +1000

    Fixed a bug that could result in having the public key added to the root user on the datamount

--- nexel/util/ssh.py
+++ nexel/util/ssh.py
@@ -62,12 +62,14 @@ def __add_key_to_data_server_async(dataserver, ssh_key, uid):
     _, stdout, stderr = \
         client.exec_command('getent passwd "%s" | cut -d: -f6' % uid)
     stderr = stderr.read()
-    if stderr != '':
+    if (stderr != '') or (stdout == ''):
         client.close()
-        logger.error(stderr)
+        logger.error("The username on the datamount is not valid: '%s'"%stderr)
         return False
     try:
         home = stdout.read().strip()
+        if not home.startswith("/home/"):
+            raise ValueError("Wrong home directory given: '%s'"%home)
     except Exception, e:
         logger.exception(e)
         client.close()
